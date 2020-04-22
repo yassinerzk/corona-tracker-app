c8c6b1231bfab3360df64054967db389f740b4a5
yassinerzk <yassinerazzouki@gmail.com>
2020-04-22 00:19:48 +0000

    final tracker app v0.1

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,34 +1,62 @@
 import React, { useState, useEffect } from "react";
-import { fetchDailyData } from "../../api";
 import { Line, Bar } from "react-chartjs-2";
+
+import { fetchDailyData } from "../../api";
+
 import styles from "./Chart.module.css";
 
-const Chart = () => {
-  const [dailyData, setDailyData] = useState([]);
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+  const [dailyData, setDailyData] = useState({});
+
   useEffect(() => {
-    const fetchAPI = async () => {
-      setDailyData(await fetchDailyData());
+    const fetchMyAPI = async () => {
+      const initialDailyData = await fetchDailyData();
+
+      setDailyData(initialDailyData);
     };
 
-    fetchAPI();
-  });
+    fetchMyAPI();
+  }, []);
 
-  const lineChart = dailyData.length ? (
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ["Infected", "Recovered", "Deaths"],
+        datasets: [
+          {
+            label: "People",
+            backgroundColor: [
+              "rgba(0, 0, 255, 0.5)",
+              "rgba(0, 255, 0, 0.5)",
+              "rgba(255, 0, 0, 0.5)",
+            ],
+            data: [confirmed.value, recovered.value, deaths.value],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current state in ${country}` },
+      }}
+    />
+  ) : null;
+
+  const lineChart = dailyData[0] ? (
     <Line
       data={{
         labels: dailyData.map(({ date }) => date),
         datasets: [
           {
-            data: dailyData.map(({ confirmed }) => confirmed),
+            data: dailyData.map((data) => data.confirmed),
             label: "Infected",
             borderColor: "#3333ff",
             fill: true,
           },
           {
-            data: dailyData.map(({ deaths }) => deaths),
+            data: dailyData.map((data) => data.deaths),
             label: "Deaths",
             borderColor: "red",
-            backgroundColor: "rgba(250,0,0,0.5)",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
             fill: true,
           },
         ],
@@ -37,9 +65,7 @@ const Chart = () => {
   ) : null;
 
   return (
-    <div className={styles.container}>
-		{lineChart}
-    </div>
+    <div className={styles.container}>{country ? barChart : lineChart}</div>
   );
 };
 
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
 
-import "./CountryPicker.module.css";
+import { fetchCountries } from "../../api";
 
-const CountryPicker = () => {
-  return <div>Country Picker</div>;
+import styles from "./CountryPicker.module.css";
+
+const Countries = ({ handleCountryChange }) => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, []);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value="">Global</option>
+        {countries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
 };
-
-export default CountryPicker;
+export default Countries;
